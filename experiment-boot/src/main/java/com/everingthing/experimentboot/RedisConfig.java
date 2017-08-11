26277800e7a7980fd78836b6249036e96f9ded17
seymour-neoby <120465271@qq.com>
2017-08-11 09:50:24 +0800

    新加入redis包

--- experiment-boot/src/main/java/com/everingthing/experimentboot/RedisConfig.java
+++ experiment-boot/src/main/java/com/everingthing/experimentboot/RedisConfig.java
@@ -14,6 +14,8 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 import java.lang.reflect.Method;
 
@@ -22,31 +24,38 @@ import java.lang.reflect.Method;
 public class RedisConfig extends CachingConfigurerSupport {
 
 	@Bean
-	public KeyGenerator keyGenerator() {
-		return new KeyGenerator() {
-			@Override
-			public Object generate(Object target, Method method, Object... params) {
-				StringBuilder sb = new StringBuilder();
-				sb.append(target.getClass().getName());
-				sb.append(method.getName());
-				for (Object obj : params){
-					sb.append(obj.toString());
-				}
-				return sb.toString();
-			}
-		};
-	}
-
-	@Bean
 	public CacheManager cacheManager(RedisTemplate redisTemplate) {
 		RedisCacheManager rcm = new RedisCacheManager(redisTemplate);
 		//设置缓存过期时间
-		//rcm.setDefaultExpiration(60);//秒
+		rcm.setDefaultExpiration(60);//秒
 		return rcm;
 	}
 
 	@Bean
 	public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory factory) {
+		/*StringRedisTemplate template = new StringRedisTemplate(factory);
+		Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
+		ObjectMapper om = new ObjectMapper();
+		om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+		om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+		jackson2JsonRedisSerializer.setObjectMapper(om);
+		template.setValueSerializer(jackson2JsonRedisSerializer);
+		template.afterPropertiesSet();*/
+
+
+		/*Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<Object>(Object.class);
+		ObjectMapper om = new ObjectMapper();
+		om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+		om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+		jackson2JsonRedisSerializer.setObjectMapper(om);
+		RedisTemplate<String, Object> template = new RedisTemplate<String, Object>();
+//		template.setConnectionFactory(redisConnectionFactory);
+		redisTemplate.setKeySerializer(jackson2JsonRedisSerializer);
+		redisTemplate.setValueSerializer(jackson2JsonRedisSerializer);
+		redisTemplate.setHashKeySerializer(jackson2JsonRedisSerializer);
+		redisTemplate.setHashValueSerializer(jackson2JsonRedisSerializer);
+		redisTemplate.afterPropertiesSet();*/
+
 		StringRedisTemplate template = new StringRedisTemplate(factory);
 		Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
 		ObjectMapper om = new ObjectMapper();
@@ -57,6 +66,26 @@ public class RedisConfig extends CachingConfigurerSupport {
 		template.afterPropertiesSet();
 		return template;
 	}
+
+
+	@Override
+	public KeyGenerator keyGenerator() {
+		System.out.println("RedisCacheConfig.keyGenerator()");
+		return new KeyGenerator() {
+			@Override
+			public Object generate(Object o, Method method, Object... objects) {
+				StringBuilder sb = new StringBuilder();
+				sb.append(o.getClass().getName());
+				sb.append(method.getName());
+				for (Object obj : objects) {
+					sb.append(obj.toString());
+				}
+				System.out.println("keyGenerator=" + sb.toString());
+				return sb.toString();
+			}
+		};
+	}
+
 
 }
 
